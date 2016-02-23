6bd8d6d51732ff88bdf138c2fda9ca30472b4d0f
Danny Pineda and Sarah Read Brown <dcorn13@gmail.com>
2016-02-22 20:56:32 -0800

    will break it but show us the rigth place for code

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
+var newWords = function (words) {
+  var moreWords = words.replace(/e/gi, '3');
+  moreWords.replace(/o/gi, '0').replace(/I/g, '1');
+};
+
+
+
 $(document).ready(function() {
   $("form#leetspeak").submit(function(event) {
     var words = $("input#words").val();
-    var newWords = words.replace(/e/gi, '3').replace(/o/gi, '0');
+    var result = newWords(words);
 
     $(".translated").text(newWords);
 
